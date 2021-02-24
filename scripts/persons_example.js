dae7e3f1c1ad662107544a9662af77a6a0c59a3d
callstack1 <73447531+callstack1@users.noreply.github.com>
2021-02-24 14:56:34 -0300

    Update persons_example.js

--- scripts/persons_example.js
+++ scripts/persons_example.js
@@ -3,31 +3,31 @@ export { persons };
 const persons = {
     "person1": {
         "name": "John",
-        "picture": "../images/rec/person1.jpeg",
+        "picture": "../Bike-Website/images/rec/person1.jpeg",
         "txt": `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultrices nibh pellentesque quam porta, eu mattis tellus
                         aliquet. Donec at justo pulvinar, finibus nibh sed, fringilla lectus.`
     },
     "person2": {
         "name": "Maria",
-        "picture": "../images/rec/person2.jpeg",
+        "picture": "../Bike-Website/images/rec/person2.jpeg",
         "txt": `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
         Morbi enim tellus, convallis aliquet iaculis sed, facilisis nec velit. Sed condimentum.`
     },
     "person3": {
         "name": "Joey",
-        "picture": "../images/rec/person3.jpeg",
+        "picture": "../Bike-Website/images/rec/person3.jpeg",
         "txt": `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
         Nunc ac velit pretium quam tincidunt aliquet id eget purus. Praesent elementum.`
     },
     "person4": {
         "name": "Carla",
-        "picture": "../images/rec/person4.jpeg",
+        "picture": "../Bike-Website/images/rec/person4.jpeg",
         "txt": `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
         Suspendisse congue diam ex, ut faucibus lacus egestas vitae. Fusce tincidunt eu.`
     },
     "person5": {
         "name": "William",
-        "picture": "../images/rec/person5.jpeg",
+        "picture": "../Bike-Website/images/rec/person5.jpeg",
         "txt": `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
         Ut fringilla, elit vitae dignissim luctus, nulla metus malesuada magna, tristique lacinia.`
     }
